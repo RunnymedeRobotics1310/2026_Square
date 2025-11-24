8529945c4dbfae90b3a21393fc004d3727945d0c
Jeff Zakrzewski <jeff@911incident.com>
2025-11-24 15:39:56 -0500

    Add driveconstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,4 +20,28 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
     public static final double DRIVER_CONTROLLER_DEADBAND = .2;
   }
+
+    public static final class DriveConstants {
+
+        public static enum DriveMode {
+            TANK, SINGLE_STICK_ARCADE, DUAL_STICK_ARCADE;
+        }
+
+        public static final int     LEFT_MOTOR_PORT               = 10;
+        public static final int     RIGHT_MOTOR_PORT              = 20;
+
+        public static final boolean LEFT_MOTOR_REVERSED           = false;
+        public static final boolean RIGHT_MOTOR_REVERSED          = true;
+
+        public static final boolean LEFT_ENCODER_REVERSED         = false;
+        public static final boolean RIGHT_ENCODER_REVERSED        = true;
+
+        public static final int     ENCODER_COUNTS_PER_REVOLUTION = 1024;
+        public static final double  ROBOT_WHEEL_DIAMETER_CMS      = 6 * 2.54;
+
+        public static final double  CMS_PER_ENCODER_COUNT         =
+                // Assumes the encoders are directly mounted on the wheel shafts
+                (ROBOT_WHEEL_DIAMETER_CMS * Math.PI) / ENCODER_COUNTS_PER_REVOLUTION;
+
+    }
 }
