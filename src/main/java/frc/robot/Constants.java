2c0c3eb735f3ea394b9299e4cd7c95ffc6b0503f
Tony Field <tony@kingsway.digital>
2025-11-24 16:25:50 -0500

    2025 files for reference

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,44 +4,68 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose.
- *
- * <p>All constants should be declared globally (i.e. public static). <br>
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose.
+ * <p>
+ * All constants should be declared globally (i.e. public static). <br>
  * Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
-  public static final class OperatorInputConstants {
+    public static final double DEFAULT_COMMAND_TIMEOUT_SECONDS = 5;
 
-    public static final int DRIVER_CONTROLLER_PORT = 0;
-    public static final double DRIVER_CONTROLLER_DEADBAND = .2;
-  }
+    public static final class OperatorInputConstants {
+
+        public static final int    DRIVER_CONTROLLER_PORT     = 0;
+        public static final double DRIVER_CONTROLLER_DEADBAND = .2;
+    }
+
+    public static final class AutoConstants {
+
+        public static enum AutoPattern {
+            DO_NOTHING, DRIVE_FORWARD, BOX
+        }
+    }
 
     public static final class DriveConstants {
 
         public static enum DriveMode {
-            TANK, SINGLE_STICK_ARCADE, DUAL_STICK_ARCADE;
+            TANK, ARCADE, SINGLE_STICK_LEFT, SINGLE_STICK_RIGHT;
         }
 
-        public static final int     LEFT_MOTOR_PORT               = 10;
-        public static final int     RIGHT_MOTOR_PORT              = 20;
+        // NOTE: Follower motors are at CAN_ID+1
+        public static final int     LEFT_MOTOR_CAN_ID    = 10;
+        public static final int     RIGHT_MOTOR_CAN_ID   = 20;
 
-        public static final boolean LEFT_MOTOR_REVERSED           = false;
-        public static final boolean RIGHT_MOTOR_REVERSED          = true;
+        public static final boolean LEFT_MOTOR_INVERTED  = false;
+        public static final boolean RIGHT_MOTOR_INVERTED = true;
 
-        public static final boolean LEFT_ENCODER_REVERSED         = false;
-        public static final boolean RIGHT_ENCODER_REVERSED        = true;
+        public static final double  CM_PER_ENCODER_COUNT = 3.503;
 
-        public static final int     ENCODER_COUNTS_PER_REVOLUTION = 1024;
-        public static final double  ROBOT_WHEEL_DIAMETER_CMS      = 6 * 2.54;
+        public static final boolean GYRO_INVERTED        = false;
 
-        public static final double  CMS_PER_ENCODER_COUNT         =
-                // Assumes the encoders are directly mounted on the wheel shafts
-                (ROBOT_WHEEL_DIAMETER_CMS * Math.PI) / ENCODER_COUNTS_PER_REVOLUTION;
+        /** Proportional gain for gyro pid tracking */
+        public static final double  GYRO_PID_KP          = 0.01;
 
+        public static final double  DRIVE_SCALING_BOOST  = 1;
+        public static final double  DRIVE_SCALING_NORMAL = .6;
+        public static final double  DRIVE_SCALING_SLOW   = .3;
+    }
+
+    public static final class CoralConstants {
+
+        public static final int ELEVATOR_MOTOR_CAN_ID = 30;
+        public static final int ARM_MOTOR_CAN_ID      = 31;
+        public static final int INTAKE_MOTOR_CAN_ID   = 32;
+    }
+
+    public static final class LightsConstants {
+
+        public static final int LED_STRING_PWM_PORT = 9;
+        public static final int LED_STRING_LENGTH   = 60;
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.auto.AutoCommand;
+import frc.robot.commands.drive.DefaultDriveCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LightsSubsystem;
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -12,13 +21,40 @@ package frc.robot;
  */
 public class RobotContainer {
 
-  // Driver and operator controllers
-  private final OperatorInput operatorInput = new OperatorInput();
+    // Subsystems
+    // Declarre the lighting subsystem first and pass it into the other subsystem
+    // constructors so that they can indicate status information on the lights
+    private final LightsSubsystem lightsSubsystem = new LightsSubsystem();
+    private final DriveSubsystem  driveSubsystem  = new DriveSubsystem(lightsSubsystem);
+    // FIXME: add the coral subsystem
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+    // Driver and operator controllers
+    private final OperatorInput   operatorInput   = new OperatorInput();
 
-    // Configure the button bindings - pass in all subsystems
-    //    operatorInput.configureButtonBindings(driveSubsystem);
-  }
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+
+        // Initialize all Subsystem default commands.
+        driveSubsystem.setDefaultCommand(
+            new DefaultDriveCommand(operatorInput, driveSubsystem));
+
+        // Configure the button bindings - pass in all subsystems
+        // FIXME: add the coral subsystem
+        operatorInput.configureButtonBindings(driveSubsystem);
+
+        // Add a trigger to flash the LEDs in sync with the
+        // RSL light for 5 flashes when the robot is enabled
+        // This can happen also if there is a brown-out of the RoboRIO.
+        new Trigger(() -> RobotState.isEnabled())
+            .onTrue(new InstantCommand(() -> lightsSubsystem.setRSLFlashCount(5)));
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return new AutoCommand(operatorInput, driveSubsystem);
+    }
 }
